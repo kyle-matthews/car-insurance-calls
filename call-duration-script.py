06930c8da6d05bc10a9c08dca11641cbf2d7d911
Kyle M <kylem61@googlemail.com>
2023-10-18 11:54:29 +0100

    fixes issues with datetime formatting.

--- call-duration-script.py
+++ call-duration-script.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datetime import datetime
+
 
 car_insurance = pd.read_csv('Car_Insurance.csv')
 
@@ -9,8 +9,8 @@ df_dropped = df.dropna()
 
 print(df_dropped.head())
 
-df_dropped['CallEnd'] = pd.to_datetime(df_dropped['CallEnd'], format='%HH::%MM::%SS')
-df_dropped['CallStart'] = pd.to_datetime(df_dropped['CallStart'], format='%HH::%MM::%SS')
+df_dropped['CallEnd'] = pd.to_datetime(df_dropped['CallEnd'], format='%H:%M:%S')
+df_dropped['CallStart'] = pd.to_datetime(df_dropped['CallStart'], format='%H:%M:%S')
 
 
 df_dropped['CallDuration'] = df_dropped['CallEnd'] - df_dropped['CallStart']
