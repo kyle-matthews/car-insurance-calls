f4e36c006a35da586096938d443af59c54334c9d
Kyle M <kylem61@googlemail.com>
2023-10-18 09:21:50 +0100

    Includes a script that can find all unique jobs from the Job column, and count the occurances of each value.

--- unique-jobs.py
+++ unique-jobs.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+# The code is reading a CSV file named 'Car_Insurance.csv' using the pandas library. It then creates a
+# DataFrame called `df` from the data in the CSV file. The `df_dropped` DataFrame is created by
+# dropping any rows that contain missing values (NaN).
+
 car_insurance = pd.read_csv('Car_Insurance.csv')
 
 df = pd.DataFrame(car_insurance)
@@ -8,3 +12,6 @@ df_dropped = df.dropna()
 
 print(df_dropped['Job'].unique())
 
+counted_jobs = df_dropped['Job'].value_counts()
+
+print(counted_jobs)
