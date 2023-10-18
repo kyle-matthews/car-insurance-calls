097f213b6073bcaaf0a573ce83632e5ce1aa7c0b
Kyle M <kylem61@googlemail.com>
2023-10-18 09:09:26 +0100

    Adds script that calculates mean age of claimants.

--- car-insurance-calls.py
+++ car-insurance-calls.py
@@ -7,3 +7,17 @@ df = pd.DataFrame(car_insurance)
 df_dropped = df.dropna()
 
 print(df_dropped)
+
+# Finding the mean age of all claimants. 
+
+total_age = 0
+customers_counter = 0
+
+for age in df_dropped['Age']:
+    total_age += age
+    customers_counter += 1
+
+
+mean_age = total_age / customers_counter 
+# Calculates the mean age is 41
+print(int(mean_age))
