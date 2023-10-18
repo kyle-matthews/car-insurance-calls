94001309f43648dd8585d45ef189443ac1864564
Kyle M <kylem61@googlemail.com>
2023-10-18 09:40:05 +0100

    Imports datetime to handle time values in the dataframe.

--- call-duration-script.py
+++ call-duration-script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 
 car_insurance = pd.read_csv('Car_Insurance.csv')
 
@@ -7,6 +8,9 @@ df = pd.DataFrame(car_insurance)
 df_dropped = df.dropna()
 
 print(df_dropped.head())
+
+call_end = datetime.strptime(df_dropped['CallEnd'], '%H::%M::%S').time()
+call_start = datetime.strptime(df_dropped['CallStart'], '%H::%M::%S').time()
 
 #df_dropped['CallDuration'] = df_dropped['CallEnd'] - df_dropped['CallStart']
 
